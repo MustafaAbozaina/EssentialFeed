54a88b4feca6345d8b166d19298f0e1e78aad968
mustafa salah eldin <programmermostafa1@gmail.com>
2021-08-23 00:35:38 +0200

    1- instead of writing url hard coded inside the RemoteFeedLoader we changed to be injected 2- remove duplication of Creating ClientSpy and RemoteFeedLoader and put them in one method return a tuple with both of them

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -21,13 +21,15 @@ protocol FeedLoader {
 
 class RemoteFeedLoader: FeedLoader {
     var client: HttpClient?
+    var url: URL?
     
-    init(client: HttpClient) {
+    init(url: URL?, client: HttpClient) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client?.get(from: URL(string: "www.aaa.com"))
+        client?.get(from: url)
     }
 }
 
@@ -51,22 +53,28 @@ class EssentialFeedTests: XCTestCase {
     
     // testing that creation of the object not loading any data
     func test_init_notLoadData() {
-        let client = HttpClientSpy()
-        let _ = RemoteFeedLoader(client: client)
+        let client = makeSUT().client
         
         XCTAssertNil(client.requestUrl)
     }
     
     func test_load_requestDataFromUrl() {
-        //arrang
-        let client = HttpClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = makeSUT().sut
+        let client = makeSUT().client
         
         // act
         sut.load()
         
         //Assert
         XCTAssertNotNil(client.requestUrl)
+    }
+    
+    private func makeSUT(url: URL? = URL(string: "www.aaa.com")) -> (sut: RemoteFeedLoader, client: HttpClientSpy) {
+        //arrang
+        let client = HttpClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
     }
     
     
