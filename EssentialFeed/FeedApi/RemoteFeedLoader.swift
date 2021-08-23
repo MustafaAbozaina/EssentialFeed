f482d566dd86f005f901d314ef80ef71ff1ce377
mustafa salah eldin <programmermostafa1@gmail.com>
2021-08-23 13:49:49 +0200

    adding the implementation of the expected Error class and a closure for firing error and for now it fires each time the completion with error to check the functionally working fine and then will add the logic. Also I've added {_ in} to let the completion as optional not required to be fired with each load method

--- EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -20,10 +20,15 @@ public class RemoteFeedLoader: FeedLoader {
         self.url = url
     }
     
-    public func load() {
+    // here's Error type, the compiler will understand that The very near class to it which is Error extends Swift.Error
+    public func load(completion:@escaping (Error) -> Void = {_ in}) {
         client?.get(from: url)
+        completion(.connectivity)
     }
-
+    // we are using Swift.Error not just (Error), to tell the compiler don't complain because the Error type we are creating differ from Error in Swift so if you wrote Error: Error -->  the compiler will trigger an error so that we used Swift.Error to let it know that
+    public enum Error: Swift.Error {
+        case connectivity
+    }
 }
 
 // <HttpClient> does not need to be a class. it's just a contract defining which external functionality the RemoteFeedLoader needs, so the protocol is a more suitable way for defining it
